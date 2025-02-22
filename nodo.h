d4a074e7d3f62dfb45cf3898c50e776265e403ba
Peloponeso31 <tanilizquierdo@proton.me>
2025-02-22 13:27:30 -0600

    Agregar docstrings a nodo.h

--- nodo.h
+++ nodo.h
@@ -6,31 +6,51 @@ typedef struct nodo {
     struct nodo * siguiente;
 } nodo;
 
-void recorrer(struct nodo * inicio)
+/**
+ * Funcion que recorre nodos e imprime el dato que contienen y la direccion de 
+ * memoria de su siguiente elemento desde uno en particular hasta llegar al 
+ * final deestos de manera incondicional.
+ * 
+ * @param nodo Direccion de memoria del nodo desde el cual se quiere recorrer.
+ */
+void recorrer(struct nodo * nodo)
 {
-    while (inicio != NULL)
+    while (nodo != NULL)
     {
         printf(
             "Dato: %d\t\tSiguiente: %p\n",
-            inicio->dato,
-            inicio->siguiente
+            nodo->dato,
+            nodo->siguiente
         );
 
-        inicio = inicio->siguiente;
+        nodo = nodo->siguiente;
     }
 }
 
-void recorrer_rec(struct nodo * inicio)
+void recorrer_recursivo(struct nodo * nodo)
 {
-    if (inicio == NULL) return;
-    
-    recorrer_rec(inicio->siguiente);
+    if (nodo == NULL) return;
     
     printf(
         "Dato: %d\t\tSiguiente: %p\n",
-        inicio->dato,
-        inicio->siguiente
+        nodo->dato,
+        nodo->siguiente
     );
+
+    recorrer_recursivo(nodo->siguiente);
+}
+
+
+/**
+ * Funcion que recorre nodos y los cuenta hasta llegar al final.
+ * 
+ * @param nodo Direccion de memoria del nodo desde el cual se quiere recorrer.
+ * @return Numero entero que representa cuantos elementos despues del nodo dado.
+ */
+int contar(struct nodo * nodo)
+{
+    if (nodo == NULL) return 0;
+    return 1 + contar(nodo->siguiente);
 }
 
 #endif
