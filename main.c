1b579d0fa727499d4e9d53992386ee620f93ab80
Peloponeso31 <tanilizquierdo@proton.me>
2025-02-22 13:28:01 -0600

    Explicacion del main.,

--- main.c
+++ main.c
@@ -4,12 +4,35 @@
 
 int main(int argc, char * argv)
 {
-    struct nodo segundo_nodo = {8, NULL};
-    struct nodo inicio = {5, &segundo_nodo};
+    // El quinto nodo es creado "in place", es decir, dentro de
+    // la misma creacion del otro nodo, esto se puede encadenar
+    // indefinidamente, lo cual no es recomendable pero nos sirve
+    // para propositos didacticos.
 
-    recorrer_rec(&inicio);
+    // Notar tambien el ampersand (&) al inicio de nodo. Esto nos
+    // da la direccion de memoria en donde vive el nuevo nodo creado
+    struct nodo cuarto_nodo = {4, &(nodo) {
+        .dato = 5,
+        .siguiente = NULL
+    }};
 
-    //int cantidad_nodos = contar(&inicio);
+    // Se crea el resto de nodos como variables, esto es para
+    // obtener su direccion de memoria despues de su creacion
+    // y poder enlazarlos mediante el ampersand (&).
+    struct nodo tercer_nodo = {3, &cuarto_nodo};
+    struct nodo segundo_nodo = {2, &tercer_nodo};
+    struct nodo inicio = {1, &segundo_nodo};
+
+    
+    // Cuando se ejecuta:
+    recorrer(&inicio);
+
+    // Se obtiene como salida:
+    // Dato: 1         Siguiente: 0000000BEAFFFC70
+    // Dato: 2         Siguiente: 0000000BEAFFFC80
+    // Dato: 3         Siguiente: 0000000BEAFFFCA0
+    // Dato: 4         Siguiente: 0000000BEAFFFC90
+    // Dato: 5         Siguiente: 0000000000000000
 
     return 0;
 }
